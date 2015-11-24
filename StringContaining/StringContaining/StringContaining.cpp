ca8172cb81e2c347da5a7f24b0742064bb5ef5c0
JinghaoX <jinghao.xu@intusurg.com>
2015-11-23 16:37:22 -0800

    String Containing: using hashing

--- StringContaining/StringContaining/StringContaining.cpp
+++ StringContaining/StringContaining/StringContaining.cpp
@@ -6,6 +6,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+// while(a) {
+//   if (b)
+//      break;
+//   c;
+// }
+
+// is equivalent to 
+// while(a and not b)
+// { c; }
+
 bool StringContainingIterate(char *s1, char *s2)
 {
 	int i, j;
@@ -125,10 +135,11 @@ bool StringContainingSortedBad(char *s1, char *s2)
 
 // algorithm: 
 // again first sort s1 and s2
-// then move j on s1, if s1[j]<s2[i], until s1[j] == s2[i]
+// then move j on s1, if s1[j]<s2[i], until s1[j] == s2[i], i.e. let j follow i's move until s1[j]==s2[i]
 // then check if j reaches s1's end, if yes, because s1[j] != s2[i], it returns false 
 // or if not, j < strlen(s1), but if s1[j]>s2[i], means s2[i] is impossible to be located in s1[j] or s1[j+1...], so also returns false
-// if j not reaches s1's end, and also s1[j] == s2[i], then increment i
+// if j not reaches s1's end, that also implies that s1[j] == s2[i], then increment i
+// if j not reaches s1's end, it's impossible that s1[j] < s2[i] because while loop already did this check
 
 bool StringContainingSorted2(char *s1, char *s2)
 {
@@ -153,13 +164,85 @@ bool StringContainingSorted2(char *s1, char *s2)
 	return true;
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+bool StringContainPrime(char *s1, char *s2)
 {
-	char s1[] = "ABDEK";
-	char s2[] = "ABDK";
+	const int p[26] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101 };
+	int f = 1;
+	for (unsigned int i = 0; i < strlen(s1); ++i)
+	{
+		int x = p[s1[i] - 'A'];
+		// this is used to detect repeated chars in s1
+		if (f % x)
+		{
+			f *= x;
+		}
+	}
+	for (unsigned int i = 0; i < strlen(s2); ++i)
+	{
+		int x = p[s2[i] - 'A'];
+		// as long as we find a number has remainer, it means it's not in s1
+		if (f % x)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool StringContainBitHash(char *s1, char *s2)
+{
+	int hash = 0;
+
+	for (unsigned int i = 0; i < strlen(s1); ++i)
+	{
+		// use bit OR, so repeated char will not be counted again
+		hash |= (1 << (s1[i] - 'A'));
+		printf("hash & = %d\n", hash);
+	}
+	// 'KJIHGFEDCBA' corresponds to
+	// '10000011011' (hash)   
+	// so if we have C in s2, which corresponds to
+	// '00000000100' (C)
+	// the & of hash and C will = 0 
+	for (unsigned int i = 0; i < strlen(s2); ++i)
+	{
+		printf("hash & = %d\n", hash&(1 << (s2[i] - 'A')));
+		if ((hash & (1 << (s2[i] - 'A'))) == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool BrotherString(char *s1, char *s2)
+{
+	// hashing may not work, unless we found a unique hash for repeated chars
+	// we should sort and compare
+	int hash1 = 0, hash2 = 0;
+	unsigned int i;
+	for (i = 0; i < strlen(s1); ++i)
+	{
+		hash1 += (1 << (s1[i] - 'A'));
+	}
+	for (i = 0; i < strlen(s2); ++i)
+	{
+		hash2 += (1 << (s2[i] - 'A'));
+	}
+	return (hash1 == hash2);
+
+}
+
+int _tmain(int argc, _TCHAR* s1rgv[])
+{
+	char s1[] = "ABCDEFG"; // "ABBDEK";
+	char s2[] = "BCDEFGA"; // "ABDKI";
 	// bool ret = StringContainingIterate2(s1, s2);
-	bool ret = StringContainingSorted2(s1, s2);
+	bool ret = StringContainBitHash(s1, s2);
 	printf("s1 contains s2? %s\n", ret? "true" : "false");
+
+	ret = BrotherString(s1, s2);
+	printf("s1 and s2 are borthers? %s\n", ret? "true" : "false");
 	return 0;
 }
 
