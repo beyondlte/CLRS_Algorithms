341dd0d5930b510a918ecb9ec45f6791acc56089
JinghaoX <jinghao.xu@intusurg.com>
2016-01-12 09:27:57 -0800

    KnapSack: optimize V&W, don't use array

--- KnapSack/KnapSack/KnapSack.cpp
+++ KnapSack/KnapSack/KnapSack.cpp
@@ -12,21 +12,25 @@ using namespace std;
 #define MyMax(a, b) (((a)>(b))?(a):(b))
 
 // int V[MAXN], W[MAXN], x[MAXN];
-int V[MAXN], W[MAXN], x[MAXN];
+int x[MAXN];
 int d[MAXN][MAXC];
 
 int main(){
 	freopen("data.in", "r", stdin);//重定向输入流
 	freopen("data.out", "w", stdout);//重定向输出流
-	int n, C;
+	int n, C, V=0, W=0;
 	while (scanf("%d %d", &n, &C) != EOF){
+		/*
 		for (int i = 0; i<n; ++i)   
 			scanf("%d %d", &V[i], &W[i]);
+		*/
 
 		for (int i = 0; i<n; ++i)   
 			x[i] = 0; //初始化打印方案
 
 		for (int i = 0; i <= n; ++i){
+			if (i>0)
+				scanf("%d %d", &V, &W);
 			for (int j = 0; j <= C; ++j){
 				if (i==0)
 					d[i][j] = i;
@@ -34,14 +38,14 @@ int main(){
 				// d[i][j] = i == 0 ? 0 : d[i - 1][j];
 
 				// if j>= V[i-1], then we can put i-1 thing in the bag or not, so we have the Max(d[i-1][j], d[i-1][j-V[i-1]]+W[i-1]
-				if (i>0 && j >= V[i - 1])
+				if (i>0 && j >= V)
 				{
-					d[i][j] = MyMax(d[i-1][j], d[i - 1][j - V[i - 1]] + W[i - 1]);
+					d[i][j] = MyMax(d[i-1][j], d[i - 1][j - V] + W);
 					// printf("d[%d][%d] = %d\n", i, j, d[i][j]);
 				}
 				// if j< V[i-1], then we don't put i-1 thing in the bag, so we have d[i][j] = d[i-1][j]
 				// if we use d[i][j] = i == 0 ? 0 : d[i - 1][j]; before, then we don't need this else if block 
-				else if (i>0 && j< V[i-1])
+				else if (i>0 && j< V)
 					d[i][j] = d[i - 1][j];
 			}
 		}
@@ -52,7 +56,7 @@ int main(){
 		for (int i = n; i>0; --i){
 			if (d[i][j] > d[i - 1][j]){
 				x[i - 1] = 1;
-				j = j - V[i - 1];
+				j = j - V;
 			}
 		}
 		for (int i = 0; i<n; ++i)   printf("%d ", x[i]);
