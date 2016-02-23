2a45482cec6d28faaa8f6c9c2565e2c3f84713a3
JinghaoX <jinghao.xu@intusurg.com>
2016-02-23 09:46:09 -0800

    DP: Coin Change, adding DP solution

--- DP_CoinChange/DP_CoinChange/DP_CoinChange.cpp
+++ DP_CoinChange/DP_CoinChange/DP_CoinChange.cpp
@@ -1,7 +1,9 @@
 // DP_CoinChange.cpp : Defines the entry point for the console application.
-//
+// http://www.geeksforgeeks.org/dynamic-programming-set-7-coin-change/
 
 #include "stdafx.h"
+#include "stdlib.h"
+
 
 int count(int S[], int m, int n)
 {
@@ -22,12 +24,45 @@ int count(int S[], int m, int n)
 	return count(S, m - 1, n) + count(S, m, n - S[m - 1]);
 }
 
+int countDP(int S[], int m, int n)
+{
+	int i, j, x, y;
+
+	// We need n+1 rows as the table is consturcted in bottom up manner using 
+	// the base case 0 value case (n = 0)
+	int **table = (int **)malloc((n + 1)*sizeof(int *));
+	for (i = 0; i < n + 1; ++i)
+		table[i] = (int *)malloc(m*sizeof(int));
+
+	// Fill the enteries for 0 value case (n = 0)
+	for (i = 0; i<m; i++)
+		table[0][i] = 1;
+
+	// Fill rest of the table enteries in bottom up manner  
+	for (i = 1; i < n + 1; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			// Count of solutions including S[j]
+			x = (i - S[j] >= 0) ? table[i - S[j]][j] : 0;
+
+			// Count of solutions excluding S[j]
+			y = (j >= 1) ? table[i][j - 1] : 0;
+
+			// total count
+			table[i][j] = x + y;
+			printf("table[%d][%d] = %d\n", i, j, table[i][j]);
+		}
+	}
+	return table[n][m - 1];
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	int s[] = { 1, 2, 5};
-	int m = 2;
-	int n = 4;
-	int ret = count(s, m, n);
+	int m = 3;
+	int n = 6;
+	int ret = countDP(s, m, n);
 	printf("total solution is %d\n", ret);
 	return 0;
 }
@@ -53,6 +88,6 @@ how to get s(6,2), repeat it again
 consider 2nd coin in or not
 s(6,2) = s(6, 1) + s(6-2, 2) = s(6,1) + s(4,2)
 S(6,1) means the 2nd coin not in, now only has one 1 coin, so it's 111111 = 1
-s(4,2) means 2nd coin is in to fill 4, it is 1111, 121, 22
+s(4,2) means 2nd coin is in to fill 4, it is 1111, 121, 22 = 3
 
 */
